cd9a461ceff9c4298560b32c90cd442f9d877ab0
saifhussain98 <saif.h98@hotmail.com>
2022-06-07 18:25:02 +0100

    created Item DAO Class

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -0,0 +1,142 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DBUtils;
+
+	public class ItemDAO implements Dao<Item> {
+
+		public static final Logger LOGGER = LogManager.getLogger();
+
+		@Override
+		public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+			Long itemsId = resultSet.getLong("itemsId");
+			String itemsName = resultSet.getString("itemsName");
+			double value = resultSet.getDouble("value");
+			return new Item(itemsId, itemsName, value);
+		}
+
+		/**
+		 * Reads all items from the database
+		 * 
+		 * @return A list of items
+		 */
+		@Override
+		public List<Item> readAll() {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					Statement statement = connection.createStatement();
+					ResultSet resultSet = statement.executeQuery("SELECT * FROM items");) {
+				List<Item> item = new ArrayList<>();
+				while (resultSet.next()) {
+					item.add(modelFromResultSet(resultSet));
+				}
+				return item;
+			} catch (SQLException e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return new ArrayList<>();
+		}
+
+		public Item readLatest() {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					Statement statement = connection.createStatement();
+					ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY itemsId DESC LIMIT 1");) {
+				resultSet.next();
+				return modelFromResultSet(resultSet);
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		/**
+		 * Creates an item in the database
+		 * 
+		 * @param item - takes in an item object. itemsId will be ignored
+		 */
+		@Override
+		public Item create(Item item) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection
+							.prepareStatement("INSERT INTO items(itemsname, value) VALUES (?, ?)");) {
+				statement.setString(1, item.getItemsName());
+				statement.setDouble(2, item.getValue());
+				statement.executeUpdate();
+				return readLatest();
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		@Override
+		public Item read(Long itemsId) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection.prepareStatement("SELECT * FROM items WHERE itemsId = ?");) {
+				statement.setLong(1, itemsId);
+				try (ResultSet resultSet = statement.executeQuery();) {
+					resultSet.next();
+					return modelFromResultSet(resultSet);
+				}
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		/**
+		 * Updates an item in the database
+		 * 
+		 * @param item - takes in an item object, the itemsId field will be used to
+		 *                 update that item in the database
+		 * @return
+		 */
+		@Override
+		public Item update(Item item) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection
+							.prepareStatement("UPDATE items SET itemsname = ?, value = ? WHERE itemsId = ?");) {
+				statement.setString(1, item.getItemsName());
+				statement.setDouble(2, item.getValue());
+				statement.setLong(3, item.getItemsId());
+				statement.executeUpdate();
+				return read(item.getItemsId());
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		/**
+		 * Deletes an item in the database
+		 * 
+		 * @param itemsId - id of the item
+		 */
+		@Override
+		public int delete(long itemsId) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection.prepareStatement("DELETE FROM items WHERE itemsId = ?");) {
+				statement.setLong(1, itemsId);
+				return statement.executeUpdate();
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return 0;
+		}
+	}
