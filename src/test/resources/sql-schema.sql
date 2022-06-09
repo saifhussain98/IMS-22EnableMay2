2792aae2eca63a85b07641f644dc6c0604981ce1
saifhussain98 <saif.h98@hotmail.com>
2022-06-09 17:51:06 +0100

    Updated SQL Schema on test resources

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -6,3 +6,31 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `itemsId` INT(11) NOT NULL AUTO_INCREMENT,
+    `itemsname` VARCHAR(40) DEFAULT NULL,
+    `value` INT(40) NOT NULL,
+    PRIMARY KEY (`itemsId`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `fkId` INT(11) NOT NULL,
+    `orderId` INT(11) NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (orderId, fkId),
+    FOREIGN KEY (fkId) REFERENCES customers(id)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`. `order_items` (
+`order_items_id` INT(11) NOT NULL AUTO_INCREMENT,
+`fkId` INT(11) NOT NULL,
+`fk_orderId` INT NOT NULL,
+`fk_itemsId` INT NOT NULL,
+`item_quantity` INT(40) NOT NULL,
+-- `total_cost_of_order` DEC(10, 2) NOT NULL,
+
+PRIMARY KEY (order_items_id),
+FOREIGN KEY (fkId) REFERENCES customers(id),
+FOREIGN KEY (fk_orderId) REFERENCES orders(orderId),
+FOREIGN KEY (fk_itemsId) REFERENCES items(itemsId)
+);
