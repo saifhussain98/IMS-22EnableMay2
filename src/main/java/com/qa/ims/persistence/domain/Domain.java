37c20abc8e94a2bb8d42e03a3053a6d91eafa1c3
saifhussain98 <saif.h98@hotmail.com>
2022-06-09 14:50:21 +0100

    Updated Domain Class to include Order_items

--- src/main/java/com/qa/ims/persistence/domain/Domain.java
+++ src/main/java/com/qa/ims/persistence/domain/Domain.java
@@ -7,7 +7,7 @@ import com.qa.ims.utils.Utils;
 
 public enum Domain {
 
-	CUSTOMER("Information about customers"), ITEM("Individual Items"), ORDER("Purchases of items"),
+	CUSTOMER("Information about customers"), ITEM("Individual Items"), ORDER("Purchases of items"), ORDER_ITEMS("Items that have been ordered"),
 	STOP("To close the application");
 
 	public static final Logger LOGGER = LogManager.getLogger();
