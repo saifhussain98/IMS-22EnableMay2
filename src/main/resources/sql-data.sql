9e84311cbb65cc9d57fce673383a567bed79df77
saifhussain98 <saif.h98@hotmail.com>
2022-06-09 17:48:13 +0100

    Updated SQL data for main resources

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,4 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`items` (`itemsname`, `value`) VALUES ('football', '10.99');
+INSERT INTO `ims`.`orders` (`fkId`) VALUES ('1');
+INSERT INTO `ims`.`order_items` (`fkId`, `fk_orderId`, `fk_itemsId`, `item_quantity`) VALUES ('1', '1', '1', '3');
